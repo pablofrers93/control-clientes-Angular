16ff510825bc51c74dc36c6b14ba5d12d9bd6cc1
pablofrers93 <pablofrers93@gmail.com>
2023-10-12 00:52:45 -0300

    nav-bar and footer

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import 'popper.js';
+import 'bootstrap';
+
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
