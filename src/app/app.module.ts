9c5181be63e657f3d8a6a1ff8bdd0377ad476a9f
pablofrers93 <pablofrers93@gmail.com>
2023-10-13 16:54:28 -0300

    clientes-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule, Settings } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +34,12 @@ import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firestore, 'control-clientes'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    FlashMessagesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+
+    production: true,
+    
+    firestore: {
+        apiKey: "AIzaSyDL_gsxWv3JT9GXs3bALw2PekPak6RJMYk",
+        authDomain: "control-clientes-dca25.firebaseapp.com",
+        databaseURL: "https://control-clientes-dca25-default-rtdb.firebaseio.com",
+        projectId: "control-clientes-dca25",
+        storageBucket: "control-clientes-dca25.appspot.com",
+        messagingSenderId: "798345669053",
+        appId: "1:798345669053:web:7dbddab91fd03447c20ad4",
+        measurementId: "G-C89V6LPMW3"
+    }
+};
